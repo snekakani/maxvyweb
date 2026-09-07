8de4e1270cde577a9a3bc09011a0a78422f206b5
snekakani <snekaasneka7@gmail.com>
2026-09-07 17:43:45 +0530

    baseurl changed

--- src/api/baseURL.ts
+++ src/api/baseURL.ts
@@ -1,2 +1,3 @@
 // src/api/baseURL.ts
-export const BASE_URL = "http://192.168.1.12:8003/api/HRMS";
+//export const BASE_URL = "http://192.168.1.12:8003/api/HRMS";
+export const BASE_URL = "https://maxvytech.in/hrms.maxvytech.in/api/HRMS";
